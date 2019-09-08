5c7e60dfccf54ec2a0f744828483f9f5d2d40eaa
DaRealFreak <steffen.keuper@web.de>
2019-09-09 00:25:12 +0200

    [TASK] update translator configurations to consist of name and an url

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,12 +2,18 @@ package config
 
 // General contains the general information about the novel
 type General struct {
-	Title       string   `yaml:"title"`
-	AltTitle    string   `yaml:"alt-title"`
-	Author      string   `yaml:"author"`
-	Cover       string   `yaml:"cover"`
-	Raw         string   `yaml:"raw"`
-	Translators []string `yaml:"translators"`
+	Title       string        `yaml:"title"`
+	AltTitle    string        `yaml:"alt-title"`
+	Author      string        `yaml:"author"`
+	Cover       string        `yaml:"cover"`
+	Raw         string        `yaml:"raw"`
+	Translators []*Translator `yaml:"translators"`
+}
+
+// Translator contains the name and website of the translators
+type Translator struct {
+	Name string `yaml:"name"`
+	Url  string `yaml:"url"`
 }
 
 // Url contains the chapter specific options like the chapter url, selector and content options
