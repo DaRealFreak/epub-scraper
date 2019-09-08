5ac32b8249c999fc5546fff096866e5792c014e3
DaRealFreak <steffen.keuper@web.de>
2019-09-08 22:56:15 +0200

    [TASK] add alt title option to yaml options

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,6 +3,7 @@ package config
 // General contains the general information about the novel
 type General struct {
 	Title       string   `yaml:"title"`
+	AltTitle    string   `yaml:"alt-title"`
 	Author      string   `yaml:"author"`
 	Cover       string   `yaml:"cover"`
 	Raw         string   `yaml:"raw"`
