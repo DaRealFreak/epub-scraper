7d6dadeedc9891a9a0d7253be746c745df01d4ad
DaRealFreak <steffen.keuper@web.de>
2019-09-09 02:06:15 +0200

    [TASK] implement getChapterContent function and implement addChapter function

--- pkg/scraper/chapters.go
+++ pkg/scraper/chapters.go
@@ -1,1 +1,27 @@
 package scraper
+
+import (
+	"strings"
+
+	"github.com/DaRealFreak/epub-scraper/pkg/config"
+	"github.com/DaRealFreak/epub-scraper/pkg/raven"
+	"github.com/PuerkitoBio/goquery"
+)
+
+func (s *Scraper) getChapterContent(chapterURL string, content config.ChapterContent) string {
+	res, err := s.session.Get(chapterURL)
+	raven.CheckError(err)
+
+	doc := s.session.GetDocument(res)
+	html, err := doc.Find(content.ContentSelector).First().Html()
+	raven.CheckError(err)
+
+	contentDoc, err := goquery.NewDocumentFromReader(strings.NewReader(html))
+	raven.CheckError(err)
+	contentDoc.Find(content.AuthorNoteEndSelector).Each(func(i int, selection *goquery.Selection) {
+		afterAuthor, err := selection.Html()
+		raven.CheckError(err)
+		html = strings.Join(strings.Split(html, afterAuthor)[1:], "")
+	})
+	return html
+}
--- pkg/scraper/main.go
+++ pkg/scraper/main.go
@@ -3,7 +3,9 @@ package scraper
 import (
 	"github.com/DaRealFreak/epub-scraper/pkg/config"
 	"github.com/DaRealFreak/epub-scraper/pkg/epub"
+	"github.com/DaRealFreak/epub-scraper/pkg/raven"
 	"github.com/DaRealFreak/epub-scraper/pkg/session"
+	"github.com/PuerkitoBio/goquery"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -27,6 +29,21 @@ func (s *Scraper) HandleFile(fileName string) {
 	}
 
 	writer := epub.NewWriter(cfg)
-	writer.AddChapter("Ruti is cute", "Hello world!")
+
+	log.SetLevel(log.DebugLevel)
+
+	// iterate through every ToC URL and append the extracted chapters
+	for _, tocURL := range cfg.Toc.URLs {
+		res, err := s.session.Get(tocURL.URL)
+		raven.CheckError(err)
+		doc := s.session.GetDocument(res)
+		doc.Find(tocURL.ChapterSelector).Each(func(i int, selection *goquery.Selection) {
+			chapterURL, exists := selection.Attr("href")
+			if !exists {
+				log.Warning("no chapter URL found")
+			}
+			writer.AddChapter(selection.Text(), s.getChapterContent(chapterURL, tocURL.ChapterContent))
+		})
+	}
 	writer.WriteEPUB()
 }
