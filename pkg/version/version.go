0a54db48f8e19ea6fdc6ae331c1f60dab5c66eb8
DaRealFreak <steffen.keuper@web.de>
2019-09-13 00:59:21 +0200

    [TASK] bump version to 2.0.0

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // VERSION program version
-const VERSION = "1.0.2"
+const VERSION = "2.0.0"
 
 // RepositoryURL repository url to check for new versions
 const RepositoryURL = "DaRealFreak/epub-scraper"
